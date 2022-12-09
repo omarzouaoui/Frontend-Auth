33a18d0b6a1622dd5b45e5b464d34b22cbc51505
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-09 17:23:20 +0100

    working on Header

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Header } from '../../components/header/Header.js'
 
 export const Home = () => {
   return (
     <div>
+        <Header/>
         <h1>Home</h1>
     </div>
   )

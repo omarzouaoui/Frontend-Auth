3e1563cded702b1b31c6b4a2173857c2543dc2e6
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-12 16:06:34 +0100

    Footer

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { Footer } from '../../components/footer/Footer.js'
 import { Header } from '../../components/header/Header.js'
+
+import "./Home.scss"
+
 
 export const Home = () => {
   return (
     <div>
         <Header/>
-        <h1>Home</h1>
+        <section className='container hero' >
+          <div className='hero-text'>
+            <h2>Authentication System</h2>
+            <p>Master Authentication and Authorization using MERN Stack.</p>
+            <p>Implement User Registration, Login, Password Reset, Social Login, User Permissions, Email Notifications etc.</p>
+            <div className='hero-buttons--flex-start'>
+              <button className='--btn--btn-danger'>
+                Register
+              </button>
+              <button className='--btn--btn-primary'>
+                Login                
+              </button>
+            </div>
+          </div>
+          <div className='hero-image'>
+
+          </div>
+        </section>
+        <Footer/>
     </div>
   )
 }

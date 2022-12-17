8d7057b47039c7913a937ee486c3523c84cd7f5b
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-17 16:45:56 +0100

    start on Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Layout } from "./components/layout/Layout";
 import { Login } from "./pages/auth/Login";
+import { Register } from "./pages/auth/Register";
 import { Home } from "./pages/home/Home";
 
 function App() {
@@ -9,15 +10,25 @@ function App() {
     <BrowserRouter>
     <Routes>
 
-      <Route path="/" element={ 
+      <Route 
+      path="/" 
+      element={ 
       <Layout>
         <Home/>
       </Layout>
     } />
     
-      <Route path="/login" element={ 
-        <Login/>
-    } />
+      <Route 
+      path="/login" 
+      element={ 
+      <Login/>
+      } />
+
+      <Route 
+      path="/register" 
+      element={ 
+      <Register/>
+      } />
   
     </Routes>
     </BrowserRouter>    

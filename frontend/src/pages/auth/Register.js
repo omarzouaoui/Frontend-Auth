1ae5672ac8422868f471c003196879720ac940d4
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-18 10:39:33 +0100

    working on password strength conditions

--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -4,6 +4,8 @@ import { Card } from '../../components/card/Card'
 import { PasswordInput } from '../../components/passwordInput/PasswordInput'
 import styles from "./auth.module.scss"
 import {TiUserAddOutline} from "react-icons/ti"
+import { FaTimes } from "react-icons/fa"
+import { BsCheck2All } from "react-icons/bs"
 
 const initialState = {
     name : "",
@@ -18,8 +20,25 @@ export const Register    = () => {
 
     const {name, email, password, password2} = formData
 
+    const [uCase, setUCase] = useState(false)
+    const [num, setNum] = useState(false)
+    const [sChar, setSChar] = useState(false)
+    const [passLength, setPassLength] = useState(false)
+
+    const timesIcon = <FaTimes color="red" size={15} />
+    const checkIcon = <BsCheck2All color="green" size={15} />
+
+    const switchIcon = (condition) => {
+        if(condition){
+            return checkIcon
+        }
+        return timesIcon
+    }
+
+
     const handleInputChange = (e) => {
-        
+        const {name, value} = e.target
+        setFormData({...formData, [name]: value})
     }
 
     const loginUser = () => {
@@ -73,11 +92,23 @@ export const Register    = () => {
                 // onChange={handleInputChange}
                 />
 
+                {/*Password Strength Check*/}
+                <Card cardClass={styles.group}>
+                    <ul className='form-list'>
+                        <li>
+                            <span className={styles.indicator}>
+                                {uCase ? checkIcon : timesIcon}
+                                &nbsp; LowerCase & UpperCase
+                            </span>
+                        </li>
+                    </ul>
+                </Card>
+
                 <button 
                 type='submit'
                 className='--btn --btn-primary --btn-block'
                 >
-                    Login
+                    Register
                 </button>
             </form>
 

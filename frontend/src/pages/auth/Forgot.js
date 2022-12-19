0d9a83d2e486be190721bb8a85348512e5633282
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-19 11:13:56 +0100

    Forgot

--- frontend/src/pages/auth/Forgot.js
+++ frontend/src/pages/auth/Forgot.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
 import { BiLogIn } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 import { Card } from '../../components/card/Card'
@@ -26,17 +27,10 @@ export const Forgot = () => {
             <div className={styles.form}>
 
             <div className='--flex-center'>
-                <BiLogIn size={35} color="#999" />
+                <AiOutlineMail size={35} color="#999" />
             </div>
             <h2>Forgot Password</h2>
-            <div className='--flex-center'>
-                <button className='--btn --btn-google'>
-                    Login With Google
-                </button>
-            </div>
-    
-            <br/>
-            <p className='--text-center --fw-bold'>or</p>
+            
             <form onSubmit={loginUser} >
 
                 <input 
@@ -46,26 +40,22 @@ export const Forgot = () => {
                 value={email} 
                 // onChange={handleInputChange}
                 />
-                <PasswordInput
-                placeholder="Password"
-                name="password"
-                value={password}
-                // onChange={handleInputChange}
-                />
+                
                 <button 
                 type='submit'
                 className='--btn --btn-primary --btn-block'
                 >
-                    Login
+                    Get Reset Email
                 </button>
+
+                <div className={styles.links} >
+                <p><Link to="/">-Home-</Link></p>
+                <p><Link to="/login">-Login-</Link></p>
+
+            </div>
+
             </form>
 
-            <Link to="/forgot">Forgot Password</Link>
-            <span className={styles.register} >
-                <Link to="/">Home</Link>
-                <p>&nbsp; Don't have an account? &nbsp;</p>
-                <Link to="/register">Register</Link>
-            </span>
             </div>  
         </Card>
 

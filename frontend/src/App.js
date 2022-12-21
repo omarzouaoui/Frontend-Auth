6a62e3d649a4c0d73b45bbfe5a35690ba2465495
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-21 21:22:55 +0100

    Reset Password Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Layout } from "./components/layout/Layout";
 import { Forgot } from "./pages/auth/Forgot";
 import { Login } from "./pages/auth/Login";
+import { LoginWithCode } from "./pages/auth/LoginWithCode";
 import { Register } from "./pages/auth/Register";
+import { Reset } from "./pages/auth/Reset";
 import { Home } from "./pages/home/Home";
 
 function App() {
@@ -35,6 +37,18 @@ function App() {
       path="/forgot" 
       element={ 
       <Forgot/>
+      } />
+
+      <Route 
+      path="/rsetPassword/:resetToken" 
+      element={ 
+      <Reset/>
+      } />
+
+      <Route 
+      path="/loginWithCode/:email" 
+      element={ 
+      <LoginWithCode/>
       } />
   
     </Routes>

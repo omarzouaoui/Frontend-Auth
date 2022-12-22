f0194d8561465015966347d0cf021a8581a9b7b9
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-22 11:31:57 +0100

    Profile & Verify Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import { Login } from "./pages/auth/Login";
 import { LoginWithCode } from "./pages/auth/LoginWithCode";
 import { Register } from "./pages/auth/Register";
 import { Reset } from "./pages/auth/Reset";
+import { Verify } from "./pages/auth/Verify";
 import { Home } from "./pages/home/Home";
+import { Profile } from "./pages/profile/Profile";
 
 function App() {
   return (
@@ -49,6 +51,22 @@ function App() {
       path="/loginWithCode/:email" 
       element={ 
       <LoginWithCode/>
+      } />
+
+      <Route 
+      path="/profile" 
+      element={ 
+        <Layout>
+          <Profile/>
+        </Layout>
+      } />
+
+      <Route 
+      path="/verify/:verificationToken" 
+      element={ 
+        <Layout>
+          <Verify/>
+        </Layout>
       } />
   
     </Routes>
---
+++ frontend/src/pages/auth/Verify.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Verify = () => {
+  return (
+    <section>
+        <div className='--center-all'>
+            <h2>Account verification</h2>
+            <p>To verify your account, click the button below ...</p>
+            <br/>
+            <button className='--btn --btn-primary' >Verify Account</button>
+        </div>
+    </section>
+  )
+}

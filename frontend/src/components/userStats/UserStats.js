d408c322d2792176f3acc8ba97b203391af28f62
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-23 07:25:49 +0100

    UserStats

--- frontend/src/components/userStats/UserStats.js
+++ frontend/src/components/userStats/UserStats.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { InfoBox } from '../infoBox/InfoBox'
+import { BiUserCheck, BiUserMinus, BiUserX } from "react-icons/bi";
+import { FaUsers } from "react-icons/fa";
 import "./UserStats.scss"
+
+//Icons
+const icon1 = <FaUsers size={40} color="#fff" />;
+const icon2 = <BiUserCheck size={40} color="#fff" />;
+const icon3 = <BiUserMinus size={40} color="#fff" />;
+const icon4 = <BiUserX size={40} color="#fff" />;
 
 export const UserStats = () => {
   return (
-    <div>UserStats</div>
+    <div className='user-summary '>
+        <h3 className='--mt --flex-center'>User Stats</h3>
+        <div className='info-summary --flex-center'>
+
+            <InfoBox icon={icon1} title={"Total Users"} count={"5"} bgColor="card1"/>
+
+            <InfoBox icon={icon2} title={"Verify Users"} count={"2"} bgColor="card2"/>
+
+            <InfoBox icon={icon3} title={"Unverified Users"} count={"3"} bgColor="card3"/>
+
+            <InfoBox icon={icon4} title={"Suspended Users"} count={"1"} bgColor="card4"/>
+
+
+        </div>
+    </div>
   )
 }
--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { PageMenu } from '../../components/PageMenu/PageMenu'
+import { UserStats } from '../../components/userStats/UserStats'
 import "./UserList.scss"
 
 export const UserList = () => {
   return (
     <section>
-        <div className='container'>
+        <div className='container '>
+
             <PageMenu/>
+            <UserStats/> 
+
+            <div className='user-list'>
+                <div className='--flex-between'>
+                    <h3></h3>
+                </div>
+            </div>
+
         </div>
     </section>
   )

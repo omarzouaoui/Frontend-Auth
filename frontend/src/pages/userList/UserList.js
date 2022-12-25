e1c8949b2632d4451db9c598ef6bcd7739b5cdaa
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-25 17:33:12 +0100

    UserList

--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { PageMenu } from '../../components/PageMenu/PageMenu'
 import { Search } from '../../components/search/Search'
 import { UserStats } from '../../components/userStats/UserStats'
+import { FaTrashAlt } from "react-icons/fa"
 import "./UserList.scss"
 
 export const UserList = () => {
@@ -13,6 +14,9 @@ export const UserList = () => {
             <UserStats/> 
 
             <div className='user-list'>
+
+                <div className='table'>
+
                 <div className='--flex-between'>
 
                     <span>
@@ -24,6 +28,39 @@ export const UserList = () => {
                     </span>
 
                 </div>
+
+                <table>
+
+                <thead>
+                    <tr>
+                        <th>s/n</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Change Role</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Omar</td>
+                        <td>omar@gmail.com</td>
+                        <td>Admin</td>
+                        <td>Change Role</td>
+                        <td>
+                            <span>
+                                <FaTrashAlt size={20} color="red" />
+                            </span>
+                        </td>
+                    </tr>
+                </tbody>
+
+               </table> 
+
+                </div>
+
             </div>
 
         </div>

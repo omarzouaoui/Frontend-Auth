9ce34488d7f45a26cacde83f2e4614cdecec2aaf
omarzouaoui <zouaouiomar80@gmail.com>
2022-12-26 08:52:38 +0100

    ChangeRole Component

---
+++ frontend/src/components/changeRole/ChangeRole.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { FaCheck } from 'react-icons/fa';
+
+export const ChangeRole = () => {
+    const [ userRole, setUserRole ] = useState("");
+  return (
+    <div className='sort' >
+        <form className='--flex-start'>
+            <select value={userRole} onChange={ (e) => setUserRole(e.target.value) }>
+                <option value="" >--select--</option>
+                <option value="subscriber" >Subscriber</option>
+                <option value="auther" >Auther</option>
+                <option value="admin" >Admin</option>
+                <option value="suspended" >Suspended</option>
+            </select>
+        </form>
+        <button className='--btn --btn-primary'>
+            <FaCheck size={15}/>
+        </button>
+    </div>
+  )
+}
